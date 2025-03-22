35f15fb0b01d38fd892a730282dbd2af1499ecbe
Taskera123 <tascalucas6@gmail.com>
2025-03-22 15:14:56 -0400

    ajust de cor de fundo

--- desaparecidos-pjc/src/components/Layout.tsx
+++ desaparecidos-pjc/src/components/Layout.tsx
@@ -4,7 +4,7 @@ import TopoInstitucional from "./TopoInstitucional";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <div className="min-h-screen bg-gray-200 text-gray-800 dark:bg-gray-900 dark:text-gray-100 transition-colors duration-300">
+    <div className="min-h-screen bg-gray-100 text-gray-800 dark:bg-gray-900 dark:text-gray-100 transition-colors duration-300">
       <TopoInstitucional />
       <main className="min-h-screen">{children}</main>
     </div>
